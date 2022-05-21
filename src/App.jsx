c658465aaeac032aca93e6a3503b2c3068c61916
Marcos Botene <marcosjbotene@gmail.com>
2022-05-21 18:41:32 -0300

    configurated fast refresh.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { useState } from 'react';
+
 import { RepositoryList } from './components';
 
 export function App() {
-  return <RepositoryList />;
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <>
+      <p>{counter}</p>
+      <button type='button' onClick={() => setCounter(counter + 1)}>
+        Increment
+      </button>
+      <RepositoryList />
+    </>
+  );
 }
