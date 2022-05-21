80349477718a7aca06ee6c115ba549b73631f1af
Marcos Botene <marcosjbotene@gmail.com>
2022-05-21 16:58:02 -0300

    configurated and using scss instead of css.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import { render } from 'react-dom';
 
-import './styles/global.css';
+import './styles/global.scss';
 import { App } from './App';
 
 render(<App />, document.getElementById('root'));
